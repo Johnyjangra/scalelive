70992fd9b682f0f02ffb97bad77aefe0222e4ca6
Your Name <sumitkhan.dhana@gmail.com>
2023-08-29 12:15:05 +0530

    contact dropdown update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,6 +5,8 @@ import icon from "../assets/images/svg/dropdown_icon.svg"
 const ContactUs = () => {
   const [isActive, setIsActive] = useState(false);
   const [selected, setIsSelected] = useState("CA");
+  const [isActive2, setIsActive2] = useState(false);
+  const [selected2, setIsSelected2] = useState("B2B");
   return (
     <>
       <section className="pb-5 position-relative" id="contact">
@@ -14,18 +16,16 @@ const ContactUs = () => {
           <Container className="mb-my-5 py-md-5">
             <h2
               className="fs_2xl text-white ff_rubik fw-medium text-center"
-              data-aos="fade-up"
-            >
-              Contact Us
+              
+            >Contact Us
             </h2>
             <p
               className="text-center fs_lg fw-normal ff_rubik txt_skyblue font_s_italic"
-              data-aos="fade-up"
-            >
-              Join Our Family Of Partners
+              
+            >Join Our Family Of Partners
             </p>
-            <div className="mw_899 text-center mx-auto" data-aos="fade-up">
-              <p className="ff_rubik text-white opacity_07 fw-normal fs_sm lh_150 mb-1">
+            <div className="mw_899 text-center mx-auto" >
+              <p className="ff_rubik text-white opacity__150 mb-1">
                 Want to access unique products at unbeatable prices? Be one of
                 the first to get their hands on the market’s ultimate
                 smartphones?
@@ -51,16 +51,15 @@ const ContactUs = () => {
               </p>
             </div>
             <Row className="align-items-center flex-column-reverse flex-xl-row mt-5 pt-xl-4 justify-content-between">
-              <Col xl={7}>
+              <Col xl={7} data-aos="fade-right" data-aos-duration="2200">
                 <form
                   action="required"
                   className="contact_form pt-lg-5 pt-xl-0 pt-4"
                 >
                   <div
                     className="d-flex gap-sm-3 gap-1 pe-sm-3 pt-sm-3  flex-column flex-sm-row"
-                    data-aos="fade-up"
-                    data-aos-delay="500"
-                  >
+                    
+                                      >
                     <Col sm={6}>
                       <span>First Name*</span>
                       <input type="text" className="w-100" />
@@ -72,9 +71,8 @@ const ContactUs = () => {
                   </div>
                   <div
                     className="d-flex gap-sm-3 gap-1 pe-sm-3 pt-sm-3  flex-column flex-sm-row"
-                    data-aos="fade-up"
-                    data-aos-delay="700"
-                  >
+                    
+                                      >
                     <Col sm={6}>
                       <span>Company Name*</span>
                       <input type="text" className="w-100" />
@@ -86,17 +84,16 @@ const ContactUs = () => {
                   </div>
                   <div
                     className="d-flex gap-sm-3 gap-1 pe-sm-3 pt-sm-3  flex-column flex-sm-row"
-                    data-aos="fade-up"
-                    data-aos-delay="900"
-                  >
+                    
+                                      >
                     <Col sm={6}>
                       <span>Email*</span>
                       <input type="email" className="w-100" />
                     </Col>
-                    <Col sm={6}>
+                    <Col sm={6} className="dropdown_input">
                       <span>Company Type*</span>
 
-                      <div className="dropdown">
+                      <div className="dropdown2">
                             <div
                               onClick={(e) => {
                                 setIsActive(!isActive);
@@ -108,20 +105,11 @@ const ContactUs = () => {
                               <img src={icon} alt="icon" />
                             </div>
                             <div
-                              className="dropdown-content"
+                              className="dropdown-content2 bg-black"
                               style={{ display: isActive ? "block" : "none" }}
                             >
                               <div
-                                onClick={(e) => {
-                                  setIsSelected(e.target.textContent);
-                                  setIsActive(!isActive);
-                                }}
-                                className="item"
-                              >
-                                CA
-                              </div>
-                              <div
-                                className="item"
+                                className="item2  opacity_100 p-1 "
                                 onClick={(e) => {
                                   setIsSelected(e.target.textContent);
                                   setIsActive(!isActive);
@@ -130,7 +118,7 @@ const ContactUs = () => {
                                 BA
                               </div>
                               <div
-                                className="item"
+                                className="item2 opacity_100 p-1"
                                 onClick={(e) => {
                                   setIsSelected(e.target.textContent);
                                   setIsActive(!isActive);
@@ -145,33 +133,59 @@ const ContactUs = () => {
                   </div>
                   <div
                     className="pt-sm-3 pt-sm-3 pt-2"
-                    data-aos="fade-up"
-                    data-aos-delay="1100"
+                    
+                    
                   >
                     <span>Buyers</span>
-                    <div className="company_type styled-select  mt-3">
-                      <select
-                        className="dropdown w-100"
-                      >
-                        <option className="bg-black" value="volvo">B2B</option>
-                        <option value="saab">A3S</option>
-                        <option value="opel">OK5</option>
-                        <option value="audi">U7S</option>
-                      </select>
-                    </div>
+                    <div className="dropdown">
+                            <div
+                              onClick={(e) => {
+                                setIsActive2(!isActive2);
+                              }}
+                              className="dropdown-btn d-flex justify-content-between"
+                            >
+                              {selected2}
+                              
+                              <img src={icon} alt="icon" />
+                            </div>
+                            <div
+                              className="dropdown-content"
+                              style={{ display: isActive2 ? "block" : "none" }}
+                            >
+                             
+                              <div
+                                className="item"
+                                onClick={(e) => {
+                                  setIsSelected2(e.target.textContent);
+                                  setIsActive2(!isActive2);
+                                }}
+                              >
+                                DA
+                              </div>
+                              <div
+                                className="item"
+                                onClick={(e) => {
+                                  setIsSelected2(e.target.textContent);
+                                  setIsActive2(!isActive2);
+                                }}
+                              >
+                                NA
+                              </div>
+                            </div>
+                          </div>
                   </div>
                   <div
                     className="pt-sm-3 pt-sm-3 pt-2"
-                    data-aos="fade-up"
-                    data-aos-delay="1300"
+                    
+                    
                   >
                     <span>Message</span>
                     <textarea className="w-100 rounded-2 ff_rubik text-white fw-normal px-3  fs_sm opacity_07"></textarea>
                   </div>
                   <div
                     className="pt-sm-4 pt-3 mt-lg-2"
-                    data-aos="fade-up"
-                    data-aos-delay="1500"
+                    
+                    
                   >
                     <button className="ff_rubik fw-normal fs_sm text-white form_btn btn_h">
                       Send Now
